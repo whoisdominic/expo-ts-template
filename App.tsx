c5d56446899c076febc4f1512de91e0a959014af
Dominic Cobb <dominicshajaa@icloud.com>
2020-10-21 14:39:45 -0700

    React Native Expo TS BoilerPlate

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import RootStack from "./screens/stacks/RootStack";
+import MainStack from "./screens/stacks/MainStack"
+
+const LandingStack = createStackNavigator();
+
+const auth: boolean = false
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      {auth? <MainStack /> : <RootStack />} 
+    </NavigationContainer>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

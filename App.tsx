72792dc9a2071006d84ad1e9cc01f38cb5c4b787
Dominic Cobb <dominicshajaa@icloud.com>
2020-11-05 12:08:57 -0800

    src file change fixed

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
 
-import RootStack from "./screens/stacks/RootStack";
-import MainStack from "./screens/stacks/MainStack"
+import RootStack from "./src/stacks/RootStack";
+import MainStack from "./src/stacks/MainStack";
 
 const LandingStack = createStackNavigator();
 
-const auth: boolean = false
+const auth: boolean = false;
 
 export default function App() {
   return (
     <NavigationContainer>
-      {auth? <MainStack /> : <RootStack />} 
+      {auth ? <MainStack /> : <RootStack />}
     </NavigationContainer>
-    )
+  );
 }
